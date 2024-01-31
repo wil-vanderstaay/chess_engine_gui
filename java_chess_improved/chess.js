6a75d210bcea32294a9fccd9366a5f0907d177c9
wvan0005 <wvan0005@student.monash.edu>
2024-01-31 23:56:40 +1100

    Copied piece square tables and implemented basic move ordering

--- java_chess_improved/chess.js
+++ java_chess_improved/chess.js
@@ -1050,6 +1050,124 @@ class Book {
 
 // BOT ----------------------------------------------------------------------------------------------------------------------
 class Bot {
+    static PIECE_VALUES = [
+        82, 337, 365, 477, 1025, 0, // opening material score
+        94, 281, 297, 512, 936, 0 // endgame material score
+    ];
+    static PIECE_POSITION_VALUES = [
+        [0, 0, 0, 0, 0, 0, 0, 0, //pawn
+            98, 134, 61, 95, 68, 126, 34, -11,
+            -6, 7, 26, 31, 65, 56, 25, -20,
+            -14, 13, 6, 21, 23, 12, 17, -23,
+            -27, -2, -5, 12, 17, 6, 10, -25,
+            -26, -4, -4, -10, 3, 3, 33, -12,
+            -35, -1, -20, -23, -15, 24, 38, -22,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ],
+        [-167, -89, -34, -49, 61, -97, -15, -107, // knight
+        -73, -41, 72, 36, 23, 62, 7, -17,
+        -47, 60, 37, 65, 84, 129, 73, 44,
+        -9, 17, 19, 53, 37, 69, 18, 22,
+        -13, 4, 16, 13, 28, 19, 21, -8,
+        -23, -9, 12, 10, 19, 17, 25, -16,
+        -29, -53, -12, -3, -1, 18, -14, -19,
+        -105, -21, -58, -33, -17, -28, -19, -23,
+        ],
+        [-29, 4, -82, -37, -25, -42, 7, -8, // bishop
+        -26, 16, -18, -13, 30, 59, 18, -47,
+        -16, 37, 43, 40, 35, 50, 37, -2,
+        -4, 5, 19, 50, 37, 37, 7, -2,
+        -6, 13, 13, 26, 34, 12, 10, 4,
+            0, 15, 15, 15, 14, 27, 18, 10,
+            4, 15, 16, 0, 7, 21, 33, 1,
+        -33, -3, -14, -21, -13, -12, -39, -21,
+        ],
+        [32, 42, 32, 51, 63, 9, 31, 43, // rook
+            27, 32, 58, 62, 80, 67, 26, 44,
+            -5, 19, 26, 36, 17, 45, 61, 16,
+            -24, -11, 7, 26, 24, 35, -8, -20,
+            -36, -26, -12, -1, 9, -7, 6, -23,
+            -45, -25, -16, -17, 3, 0, -5, -33,
+            -44, -16, -20, -9, -1, 11, -6, -71,
+            -19, -13, 1, 17, 16, 7, -37, -26,
+        ],
+        [-28, 0, 29, 12, 59, 44, 43, 45, // queen
+        -24, -39, -5, 1, -16, 57, 28, 54,
+        -13, -17, 7, 8, 29, 56, 47, 57,
+        -27, -27, -16, -16, -1, 17, -2, 1,
+        -9, -26, -9, -10, -2, -4, 3, -3,
+        -14, 2, -11, -2, -5, 2, 14, 5,
+        -35, -8, 11, 2, 8, 15, -3, 1,
+        -1, -18, -9, 10, -15, -25, -31, -50,
+        ],
+        [-65, 23, 16, -15, -56, -34, 2, 13, // king
+            29, -1, -20, -7, -8, -4, -38, -29,
+        -9, 24, 2, -16, -20, 6, 22, -22,
+        -17, -20, -12, -27, -30, -25, -14, -36,
+        -49, -1, -27, -39, -46, -44, -33, -51,
+        -14, -14, -22, -46, -44, -30, -15, -27,
+            1, 7, -8, -64, -43, -16, 9, 8,
+        -15, 36, 12, -54, 8, -28, 24, 14,
+        ],
+        // Endgame positional piece scores //
+        [0, 0, 0, 0, 0, 0, 0, 0, //pawn
+            178, 173, 158, 134, 147, 132, 165, 187,
+            94, 100, 85, 67, 56, 53, 82, 84,
+            32, 24, 13, 5, -2, 4, 17, 17,
+            13, 9, -3, -7, -7, -8, 3, -1,
+            4, 7, -6, 1, 0, -5, -1, -8,
+            13, 8, 8, 10, 13, 0, 2, -7,
+            0, 0, 0, 0, 0, 0, 0, 0,
+        ],
+        [-58, -38, -13, -28, -31, -27, -63, -99, // knight
+        -25, -8, -25, -2, -9, -25, -24, -52,
+        -24, -20, 10, 9, -1, -9, -19, -41,
+        -17, 3, 22, 22, 22, 11, 8, -18,
+        -18, -6, 16, 25, 16, 17, 4, -18,
+        -23, -3, -1, 15, 10, -3, -20, -22,
+        -42, -20, -10, -5, -2, -20, -23, -44,
+        -29, -51, -23, -15, -22, -18, -50, -64,
+        ],
+        [-14, -21, -11, -8, -7, -9, -17, -24, // bishop
+        -8, -4, 7, -12, -3, -13, -4, -14,
+            2, -8, 0, -1, -2, 6, 0, 4,
+        -3, 9, 12, 9, 14, 10, 3, 2,
+        -6, 3, 13, 19, 7, 10, -3, -9,
+        -12, -3, 8, 10, 13, 3, -7, -15,
+        -14, -18, -7, -1, 4, -9, -15, -27,
+        -23, -9, -23, -5, -9, -16, -5, -17,
+        ],
+        [13, 10, 18, 15, 12, 12, 8, 5, // rook
+            11, 13, 13, 11, -3, 3, 8, 3,
+            7, 7, 7, 5, 4, -3, -5, -3,
+            4, 3, 13, 1, 2, 1, -1, 2,
+            3, 5, 8, 4, -5, -6, -8, -11,
+            -4, 0, -5, -1, -7, -12, -8, -16,
+            -6, -6, 0, 2, -9, -9, -11, -3,
+            -9, 2, 3, -1, -5, -13, 4, -20,
+        ],
+        [-9, 22, 22, 27, 27, 19, 10, 20, // queen
+        -17, 20, 32, 41, 58, 25, 30, 0,
+        -20, 6, 9, 49, 47, 35, 19, 9,
+            3, 22, 24, 45, 57, 40, 57, 36,
+        -18, 28, 19, 47, 31, 34, 39, 23,
+        -16, -27, 15, 6, 9, 17, 10, 5,
+        -22, -23, -30, -16, -16, -23, -36, -32,
+        -33, -28, -22, -43, -5, -32, -20, -41,
+        ],
+        [-74, -35, -18, -18, -11, 15, 4, -17, // king
+        -12, 17, 14, 17, 17, 38, 23, 11,
+            10, 17, 23, 15, 20, 45, 44, 13,
+        -8, 22, 24, 27, 26, 33, 26, 3,
+        -18, -4, 21, 24, 27, 23, 9, -11,
+        -19, -3, 11, 21, 23, 16, 7, -9,
+        -27, -11, 4, 13, 14, 4, -5, -17,
+        -53, -34, -21, -11, -28, -14, -24, -43
+        ]
+    ];
+
+    static MAX_PLY = 64;
+
     constructor(board, timer, useBook = true) {
         this.board = board;
         this.timer = timer;
@@ -1059,42 +1177,87 @@ class Bot {
         this.ply = 0;
         this.nodes = 0;
         this.best_move = 0;
+
+        this.historyMoves; // [turn][source][target]
+        this.killerMoves; // [killer index][ply]  
+        this.resetSearchTables();
+    }
+
+    resetSearchTables() {
+        this.historyMoves = Array.from(Array(2), () => Array.from(Array(64), () => Array(64).fill(0)));
+        this.killerMoves = Array.from(Array(2), () => Array(Bot.MAX_PLY));
+
+        this.nodes = 0;
+        this.best_move = 0;
     }
 
     evaluate() {
-        let pieceValues = [100, 300, 350, 500, 800, 0];
         let res = 0;
         for (let i = 0; i < 6; i++) {
-            res += pieceValues[i] * count_bits(this.board.bitboards[i]);
-            res -= pieceValues[i] * count_bits(this.board.bitboards[i + 6]);
+            let white = copy_bitboard(this.board.bitboards[i]);
+            while (bool_bitboard(white)) {
+                let square = pop_lsb_index(white);
+                res += Bot.PIECE_VALUES[i] + Bot.PIECE_POSITION_VALUES[i][square];
+            }
+            let black = copy_bitboard(this.board.bitboards[i + 6]);
+            while (bool_bitboard(black)) {
+                let square = pop_lsb_index(black);
+                square = (56 - (square & 56)) + (square & 7)
+                res -= Bot.PIECE_VALUES[i] + Bot.PIECE_POSITION_VALUES[i][square];
+            }
         }
         return [res, -res][this.board.turn];
     }
 
-    score_move(move) {
-        let res = 0;
-        let target = get_move_target(move);
+    score_move(move, quiescenceSearch = false) {
         let piece = get_move_piece(move) % 6;
-        res += 6 - piece;
+        let source = get_move_source(move);
+        let target = get_move_target(move);
+        let capture = get_move_capture(move);
 
-        let att_piece = this.board.is_square_attacked(target, this.board.turn ^ 1);
-        if (att_piece) {
-            if (piece == 5) { return -150; } // moving into check
-            if (piece > att_piece) { // attacked by lesser piece
-                res += (-piece << 3) + att_piece;
-            }
+        let res = 0;
+        if (capture) {
+            let capturePiece = this.board.squares[target] - 1;
+            let captureValue = Bot.PIECE_VALUES[piece] - Bot.PIECE_VALUES[capturePiece % 6];
+            // if opponent can recapture do below. else just +800
+            res += [20000, 80000][+(captureValue > 0)] + captureValue;
         }
 
-        if (get_move_capture(move)) {
-            let cap_piece = this.board.squares[target];
-            if (!att_piece) { res += 20; } // free piece
-            res += ((cap_piece - piece) << 2);
+        if (!piece) {
+            if (get_move_promote(move) && !capture) {
+                res += 60000;
+            }
+        } else if (piece != 5) {
+            res += Bot.PIECE_POSITION_VALUES[piece][target] - Bot.PIECE_POSITION_VALUES[piece][source];
+            // if opponent can capture with pawn -50, if with other piece -25
+        }
+
+        if (!capture) {
+            if (!quiescenceSearch) {
+                if (move == this.killerMoves[0][this.ply]) {
+                    res += 40000;
+                } else if (move == this.killerMoves[1][this.ply]) {
+                    res += 30000;
+                }
+            }
+            res += this.historyMoves[this.board.turn][source][target];
         }
         return res;
     }
 
-    generate_ordered_moves(onlyCaptures = false) {
-        return this.board.generate_moves(onlyCaptures).map(move => [this.score_move(move), move]).sort((a, b) => b[0] - a[0]).map(item => item[1]);
+    generate_ordered_moves(quiescenceSearch = false) {
+        let moves = this.board.generate_moves(quiescenceSearch);
+        let moveScores = [];
+        for (let i = 0; i < moves.length; i++) {
+            let move = moves[i];
+            moveScores.push([this.score_move(move, quiescenceSearch), move]);
+        }
+        let sortedMoves = moveScores.sort((a, b) => b[0] - a[0]);
+        let res = [];
+        for (let i = 0; i < sortedMoves.length; i++) {
+            res.push(sortedMoves[i][1]);
+        }
+        return res;
     }
 
     search_captures(depth, alpha, beta) {
@@ -1132,8 +1295,11 @@ class Bot {
         this.nodes++;
 
         if (depth == 0) { return this.search_captures(8, alpha, beta); }
+        if (depth == 1 && this.ply > 4) {
+            let x = 1;
+        }
         let moves = this.generate_ordered_moves();
-        if (!moves.length) { return -9999 + this.ply; }
+        if (!moves.length) { return -10000 + this.ply; }
         for (let i = 0; i < moves.length; i++) {
             let move = moves[i];
             this.board.do_move(move)
@@ -1144,21 +1310,29 @@ class Bot {
             this.board.undo_move();
 
             if (score >= beta) { // oppenent response too strong, snip
+                if (!get_move_capture(move)) {
+                    this.killerMoves[1][this.ply] = this.killerMoves[0][this.ply];
+                    this.killerMoves[0][this.ply] = move;
+                }
                 return beta;
             }
             if (score > alpha) {
-                if (this.ply == 0) { this.best_move = move; }
+                if (!get_move_capture(move)) {
+                    this.historyMoves[this.board.turn][get_move_source(move)][get_move_target(move)] += depth * depth;
+                }
+                if (this.ply == 0) {
+                    this.best_move = move;
+                    console.log(get_move_uci(move));
+                }
                 alpha = score;
             }
 
-            if (depth > 2 && this.timer.elapsed_this_turn() > allocatedTime) { return alpha; }
+            if (this.timer.elapsed_this_turn() > allocatedTime) { return alpha; }
         }
         return alpha;
     }
 
     think() {
-        this.best_move = 0;
-        this.nodes = 0;
         if (this.book.following) {
             let move = this.book.bookMove(this.board.moves);
             if (move) {
@@ -1170,8 +1344,9 @@ class Bot {
             this.book = { "following": false }; // clear space
         }
 
-        let allocatedTime = this.timer.remaining() / 8;
+        this.resetSearchTables();
         let res = 0;
+        let allocatedTime = this.timer.remaining() / 8;
         for (let depth = 1; this.timer.elapsed_this_turn() <= allocatedTime / 5; depth++) {
             for (let window = 40; ;) {
                 let alpha = res - window;
@@ -2436,7 +2611,7 @@ class Game {
         this.board = new Board(this.fen);
         this.moves = [];
 
-        Game.PLAYER_TIMER_INTERVALS.map((item) => clearInterval(Game.PLAYER_TIMER_INTERVALS.pop()));
+        Game.PLAYER_TIMER_INTERVALS.map(() => clearInterval(Game.PLAYER_TIMER_INTERVALS.pop()));
 
         this.playerTimer = new Timer("playerTimer", timer, timerIncrement, true);
         this.botTimer = new Timer("botTimer", timer, timerIncrement, true);
@@ -2693,7 +2868,7 @@ class Game {
         let last_move_target = get_move_target(last_move);
         let legal_moves = this.board.generate_moves().map(move => [get_move_source(move), get_move_target(move)]);
 
-        let indicies = new Array(64).fill(0).map((item, index) => index);
+        let indicies = Array.from(Array(64), (item, index) => index);
         if (!updateAll && last_move) {
             // Update pieces affected by last move
             indicies = [last_move_source, last_move_target];
